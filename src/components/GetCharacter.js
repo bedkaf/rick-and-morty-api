5106a3d0f0abfd01e8f47ea092efd6938b1b7f56
Hamilton B. Mosquera <hammosquera@gmail.com>
2022-08-24 22:42:41 -0500

    Create card and characters

---
+++ src/components/GetCharacter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+
+const GetCharacter = ({character = []}) => {
+  return (
+    <div className="row" style={{minWidth}}>
+      {
+        character.map((item, index) => (
+          <div key={index} className="col">
+            <Card item={item}/>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default GetCharacter;
